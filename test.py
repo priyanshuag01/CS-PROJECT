23460297af9bfa4e513b8fc120f46cd274e792a9
dhanusree23 <136178971+dhanusree23@users.noreply.github.com>
2023-08-04 21:22:21 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,169 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def tcs():
+    df = pd.read_csv("tcs.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("tcs stock")
+    plt.show()
+tcs()
+def infy():
+    df = pd.read_csv("infy.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("infosys stock")
+    plt.show()
+infy()
+def techm():
+    df = pd.read_csv("techm.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("date")
+    plt.ylabel("close")
+    plt.plot(x,y)
+    plt.title("tech mahindra stock")
+    plt.show()
+techm()
+def bharat():
+    df = pd.read_csv("bharat.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("bharat stock")
+    plt.show()
+bharat()
+def wipro():
+    df = pd.read_csv("wipro.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("wipro stock")
+    plt.show()
+wipro()
+def kotak():
+    df = pd.read_csv("kotak.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("kotak stock")
+    plt.show()
+kotak()
+def icici():
+    df = pd.read_csv("icicibank.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("icici bank stock")
+    plt.show()
+icici()
+def sbi():
+    df = pd.read_csv("sbibank.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("sbibank stock")
+    plt.show()
+sbi()
+def hdfc():
+    df = pd.read_csv("hdfcbank.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("hdfc stock")
+    plt.show()
+sbi()
+def axis():
+    df = pd.read_csv("axisbank.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("axis stock")
+    plt.show()
+axis()
+def sun():
+    df = pd.read_csv("sunpharma.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("sunpharma stock")
+    plt.show()
+sun()
+def apollo():
+    df = pd.read_csv("apollo.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("apollo stock")
+    plt.show()
+apollo()
+def cipla():
+    df = pd.read_csv("cipla.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("cipla stock")
+    plt.show()
+cipla()
+def divislab():
+    df = pd.read_csv("divislab.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("sbibank stock")
+    plt.show()
+divislab()
+def dreddy():
+    df = pd.read_csv("dreddy.csv")
+    plt.style.use("bmh")
+    x = df['Date']
+    y = df["Close"]
+    plt.xlabel("Date")
+    plt.ylabel("Close")
+    plt.plot(x,y)
+    plt.title("sbibank stock")
+    plt.show()
+dreddy()
+
